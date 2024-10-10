1cc86773c73811a48374f872dcbe07d7e772db43
Cindy luckyta <cindyluckyta118@gmail.com>
2024-10-10 15:21:27 +0700

    Add files via upload

---
+++ Switch.php
@@ -0,0 +1,19 @@
+<?php
+$nilai = 90;
+switch($nilai){
+    case 90:
+        echo "Nilai anda A";
+        break;
+    case 80:
+        echo "Nilai anda B";
+        break;
+    case 70:
+        echo "Nilai anda C";
+        break;
+    case 60:
+        echo "Nilai anda D";
+    default:
+        echo "Nilai anda E";
+        break;            
+}
+?>
